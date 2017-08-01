b921caae955172387e88a210de9e5ae722945970
czxs <minxiang-516@163.com>
2017-08-01 15:13:31 +0800

    new version
    new version

--- main.go
+++ main.go
@@ -1,24 +1,28 @@
 package main
-
 import (
-	"client"
-	"flag"
-	"fmt"
 	"g"
+	"fmt"
+	"flag"
 	"http"
-	"server"
+	"client"
 	"proxy"
 	"reader"
 )
 
+
 var (
-	cfg     = flag.String("c", "cfg.json", "configuration file")
-	syncSer = flag.Bool("server", false, "server mode")
-	syncCli = flag.Bool("client", false, "server mode")
-	syncPro = flag.Bool("proxy", false, "server mode")
+	cfg = flag.String("c","cfg.json","configuration file")
+	syncSer = flag.Bool( "server",false,"server mode")
+	syncCli = flag.Bool( "client",false,"server mode")
+	syncPro = flag.Bool( "proxy",false,"server mode")
 )
 
-func main() {
+
+
+
+
+func main(){
+
 
 	flag.Parse()
 
@@ -27,21 +31,21 @@ func main() {
 	forever := make(chan bool)
 	if *syncSer || g.Config().Reciver.Client.Enable {
 		fmt.Println("start http server and fileserver server ...")
-		go http.Start()
+		go http.Start()	
 		go proxy.ProxyStorage()
 		go reader.ServerInit()
-		go server.TcpConn.InitTcpConnPool()
 	}
 
-	if *syncCli || g.Config().Reciver.Server.Enable {
+	if *syncCli ||g.Config().Reciver.Server.Enable {
 		fmt.Println("start fileserver client ...")
-		if *syncPro || g.Config().Reciver.Server.Proxy {
+		if *syncPro || g.Config().Reciver.Server.Proxy{
 			client.Proxyserver = true
 			fmt.Println("start proxyserver ...")
 		}
 		go client.StartClient()
 	}
-
+	
 	<-forever
+
 
 }
